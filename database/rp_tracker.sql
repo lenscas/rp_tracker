29a8a1e901ffd305d2ec33d12643605568513980
lenscas <lenscas@lenscas-laptop-2>
2016-06-30 14:08:25 +0200

    added an updated version of the database

--- database/rp_tracker.sql
+++ database/rp_tracker.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 23, 2016 at 05:15 PM
+-- Generation Time: Jun 30, 2016 at 02:07 PM
 -- Server version: 5.5.49-0+deb8u1
--- PHP Version: 5.6.20-0+deb8u1
+-- PHP Version: 5.6.22-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -22,6 +22,25 @@ SET time_zone = "+00:00";
 CREATE DATABASE IF NOT EXISTS `rp_tracker` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `rp_tracker`;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `abilities`
+--
+
+DROP TABLE IF EXISTS `abilities`;
+CREATE TABLE IF NOT EXISTS `abilities` (
+`id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `cooldown` int(11) NOT NULL,
+  `text` longtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Truncate table before insert `abilities`
+--
+
+TRUNCATE TABLE `abilities`;
 -- --------------------------------------------------------
 
 --
@@ -43,9 +62,18 @@ CREATE TABLE IF NOT EXISTS `characters` (
   `accuracy` int(11) NOT NULL,
   `magicalSkill` int(11) NOT NULL,
   `magicalDefence` int(11) NOT NULL,
-  `peronality` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `personality` longtext NOT NULL,
+  `code` varchar(7) NOT NULL,
+  `isMinion` tinyint(1) NOT NULL,
+  `agility` int(11) NOT NULL,
+  `notes` longtext
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
+--
+-- Truncate table before insert `characters`
+--
+
+TRUNCATE TABLE `characters`;
 -- --------------------------------------------------------
 
 --
@@ -58,8 +86,13 @@ CREATE TABLE IF NOT EXISTS `players` (
   `userId` varchar(255) NOT NULL,
   `rpId` int(11) NOT NULL,
   `is_GM` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
+--
+-- Truncate table before insert `players`
+--
+
+TRUNCATE TABLE `players`;
 -- --------------------------------------------------------
 
 --
@@ -74,9 +107,15 @@ CREATE TABLE IF NOT EXISTS `rolePlays` (
   `isPrivate` int(11) NOT NULL,
   `startingStatAmount` int(11) NOT NULL,
   `startingAbilityAmount` int(11) NOT NULL,
-  `description` longtext NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
+  `description` longtext NOT NULL,
+  `creator` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
+--
+-- Truncate table before insert `rolePlays`
+--
+
+TRUNCATE TABLE `rolePlays`;
 -- --------------------------------------------------------
 
 --
@@ -94,8 +133,26 @@ CREATE TABLE IF NOT EXISTS `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- Truncate table before insert `users`
+--
+
+TRUNCATE TABLE `users`;
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `email`, `hasActivated`, `activationCode`) VALUES
+('99b4cfd00e2e6acfdef8d73a4be47193f57c2f21', 'root', 'CYnWDbrGJPDj+E6hwMgbgLuqlIuS6VuyDi6FCwQEtWWENzgxYZQn1en4RYCEvHy88/Vc6LMOsCitAiF7xCuZ6w==', 'lenscas@localhost', 1, '');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `abilities`
+--
+ALTER TABLE `abilities`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `characters`
@@ -126,20 +183,25 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `abilities`
+--
+ALTER TABLE `abilities`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
 -- AUTO_INCREMENT for table `characters`
 --
 ALTER TABLE `characters`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `players`
 --
 ALTER TABLE `players`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=2;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `rolePlays`
 --
 ALTER TABLE `rolePlays`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
